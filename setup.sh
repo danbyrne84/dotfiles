02fde69e649aeddb301f2f73adda8847f6bb23c2
Dan Byrne <dan@www.danielbyrne.net>
2014-01-17 17:26:50 +0000

    Added htop

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Installing necessary packages"
-apt-get install --ignore-missing git vim build-essential
+apt-get install --ignore-missing git vim build-essential htop
 
 echo "Cloning dotfiles repo"
 mkdir ~/src/dotfiles
